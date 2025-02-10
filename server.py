1d5838ddc4b9bfb9c4d40bfdff31ef1e1b9b4fc8
Yach <135635030+y1ach@users.noreply.github.com>
2025-02-10 22:10:53 +0300

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 ADMIN_CREDENTIALS = {
     "username": "admin",
-    "password": "EIUHEEWoıhıhewOUyıYODTUFvUYfEFWHE29237RGUIŞWEUFGQWE"  # Burayı daha güçlü bir şey yap
+    "password": "adminyigitsos123sosyigitadmin"  # Burayı daha güçlü bir şey yap
 }
 
 posts = []
